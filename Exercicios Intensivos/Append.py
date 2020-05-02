b8dbb88ff2c2d4a58b5b2ee30046864d64411d4c
Ageu Silva <39139593+Ageursilva@users.noreply.github.com>
2020-05-01 22:42:33 -0300

    Add files via upload

---
+++ Exercicios Intensivos/Append.py
@@ -0,0 +1,6 @@
+carros =[]
+
+carros.append(input('Digite o nome de um carro: '))
+carros.append(input('Digite o nome de um carro: '))
+
+print(carros)
---
+++ Sauve.py
@@ -0,0 +1,9 @@
+names = ['Ageu','Luan','Zé','Kener','vitor','Wesley']
+
+print('Saudações',names[0])
+print('Saudações',names[1])
+print('Saudações',names[2])
+print('Saudações',names[3])
+print('Saudações',names[4])
+print('Saudações',names[5])
+
